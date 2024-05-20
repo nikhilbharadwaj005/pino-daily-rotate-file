57cc7224d43cd43fed3eb2a8e4a983fde62164c5
Nikhil Bharadwaj <nikhilbharadwaj@Nikhils-MacBook-Air.local>
2024-05-20 16:01:59 +0530

    added env variable log in both file and terminal at same time

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-'use strict';
-const path = require('path')
-const pino = require('pino');
+"use strict";
+const path = require("path");
+const pino = require("pino");
 
 // DEBUG < INFO < WARN < ERROR < FATAL
 
+const targets = [
+  {
+    level: process.env.LOGGING_LEVEL || "info",
+    target: path.resolve(__dirname + "/transport-stream.js"),
+  },
+];
+
+if (process.env.PINO_TO_CONSOLE == "true") {
+  targets.unshift({
+    level: process.env.LOGGING_LEVEL || "info",
+    target: "pino-pretty",
+  });
+}
+
 const log = pino({
-  level: process.env.LOGGING_LEVEL || 'info',
+  level: process.env.LOGGING_LEVEL || "info",
   mixin(_context, level) {
-    return { 'level-label': log.levels.labels[level] }
+    return { "level-label": log.levels.labels[level] };
   },
   transport: {
-    targets: [
-      { level: process.env.LOGGING_LEVEL || 'info',
-        target: path.resolve(__dirname+'/transport-stream.js')
-      }
-    ]
-  }
+    targets,
+  },
 });
 
-
-module.exports = log
+module.exports = log;
